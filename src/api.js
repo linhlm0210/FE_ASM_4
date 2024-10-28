717e795c2bd53a2285143abeaa461dcdcf7fb6e2
linhlm <lammylinh7012@gmail.com>
2024-10-28 23:19:33 +0700

    final

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: BACKEND_URL,
 });
 
 // Thêm token vào header của mọi request
